c77e3267ae195c42b25e66f986e8f4f50bacb235
shellywilliamsreichert802 <154470623+shellywilliamsreichert802@users.noreply.github.com>
2024-01-21 21:46:23 -0600

    Finally! It's Happened! All 10 Passed

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,9 +12,10 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions = ["Who was the first American woman in space?", "True or false: 5 kilometer == 5000 meters?", "(5 + 3)/2 * 10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45]', what entry is at index 2?", "What is the minimum crew size for the ISS?"];
-let correctAnswers = ["Sally Ride" , "true", "40", "Trajectory", 3];
-let candidateAnswers = [""];
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride" , "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
+
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -24,22 +25,17 @@ function askForName() {
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer   candidateAnswer = input.question(question)
 for (let i = 0; i < questions.length; i++) {
-  console.log(questions[i]);
+let userAnswers = input.question(questions[i]);
+  candidateAnswers.push(userAnswers);
+}
 }
 
-}
-for (let i = 0; i < candidateAnswers.length; i++) {
-   console.log(candidateAnswers[i]);  
-}
+//}for (let i = 0; i < candidateAnswers.length; i++) {   console.log(candidateAnswers[i]);  }
 
 function gradeQuiz(candidateAnswers) {
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly /  console.log("Correct Answer: Sally Ride");
-// 
-if (candidateAnswers == correctAnswers) { 
-  console.log(`You are correct! The answers are ${correctAnswers}`);
-} else {
-  console.log(`Incorrect you answered: ${candidateAnswers}`);
-}
+  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly /  console.log("Correct Answer: Sally Ride");if (candidateAnswers == correctAnswers) { console.log(`You are correct! The answers are ${correctAnswers}`);} else { console.log(`Incorrect you answered: ${candidateAnswers}`);}//
+
+console.log(`You answered: ${candidateAnswers}. The correct answers are: ${correctAnswers}.`)
 
   let grade;  
   //TODO 3.2 use this variable to calculate the candidates score.
